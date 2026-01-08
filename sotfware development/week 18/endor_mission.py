001a551177e1ce53958297376483293437c14c8a
Ruiz, Lucas <Lucasruiz200928@gmail.com>
2026-01-08 09:51:26 -0500

    Add files via upload

---
+++ sotfware development/week 18/endor_mission.py
@@ -0,0 +1,18 @@
+# Ruiz, Lucas
+
+def rebel_squad():
+    print("\n---Battle of Endor Mission ---\n")
+
+    print("3 rebel squads are participarting in the mission")
+    reble_squad1 = "red squad"
+    reble_squad2 = "blue squad"
+    reble_squad3 = "green squad"
+    print("The squads that are participating is: " + reble_squad1 + ", " + reble_squad2 + ", " + reble_squad3)
+
+rebel_squad()
+
+def red_squad():
+    integer1 = 80
+    if integer1 >= 75:
+        print("Red squad has entered imperial targets")
+red_squad()
